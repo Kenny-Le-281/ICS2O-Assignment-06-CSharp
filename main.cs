cf32f20ea560b742a1de1603c3158e817ebb67f6
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-21 16:35:21 +0000

    changes in main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,38 @@
 using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static async Task Main()
+    {
+
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://animechan.vercel.app/api/random"
+        );
+        // Console.WriteLine(response);
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        // Console.WriteLine(jsonAsDictionary);
+        Console.WriteLine("");
+        JsonNode resultNode = JsonNode.Parse(response)!;
+        // Console.WriteLine(forecastNode);
+        JsonNode animeNode = resultNode!["anime"]!;
+        // Console.WriteLine(weatherNode);
+        JsonNode characterNode = resultNode!["character"]!;
+        // Console.WriteLine(weatherNode);
+        JsonNode quoteNode = resultNode!["quote"]!;
+        
+        Console.WriteLine("Anime name: " + animeNode);
+        Console.WriteLine("");
+        Console.WriteLine("Character name: " + characterNode);
+        Console.WriteLine("");
+        Console.WriteLine("Quote: " + '"' + quoteNode + '"');
+        Console.WriteLine("");
+        Console.WriteLine("\nDone.");
+    }
 }
